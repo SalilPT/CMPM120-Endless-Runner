47ddd55be349a35cf52a8c03a0927e8c569a33eb
Gus700 <gcruzmar@ucsc.edu>
2022-05-05 19:25:02 -0700

    added title screen and credits scene with button functionality

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -6,10 +6,58 @@ class Credits extends Phaser.Scene {
     init() {
 
     }
-
+    preload() {
+        //this.load.spritesheet("volcanicBackground", "./assets/background sprite sheet (3 frames) 384x128.png", {frameWidth: 384/3, frameHeight: 128});
+    }
     create() {
-        // Draw text for credits
+        console.log("started the credits scene")
+        this.myBackground = this.add.tileSprite(0, -globalGame.config.height, globalGame.config.width, globalGame.config.height*2, "volcanicBackground").setOrigin(0);
+        this.myBackgroundTimer = this.createTileSpriteAnimTimer(this.myBackground, 3, 4);
+        // credit text configuration
+        let creditsConfig = {
+            fontFamily: 'Courier',
+            fontSize: '30px',
+            fontStyle: 'Bold',
+            //backgroundColor: '#FFFFFF',
+            color: '#FFFFFF',
+            align: 'left',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        
+        // show menu text
+        this.add.text(globalGame.config.width/2, globalGame.config.height/2, 'Art by:\nMilesKatlin\n\nProgramming by:\nSalil Tantamjarik\nGustavo Cruz', creditsConfig).setOrigin(0.5);
+        //show title button
+        this.titleButton = this.add.sprite(globalGameConfig.width/8, globalGameConfig.height/1.15, "titleButton");
+        this.titleButton.setInteractive();
+        //titleButton button functionality
+        this.titleButton.on("pointerdown", () => {
+            console.log("Clicked on titleButton in Credits scene");
+            this.scene.sleep("creditsScene")
+            this.scene.wake("menuScene");
+            this.scene.wake("playScene");
+        });
 
         // Assign event to transition back to menu
+    }
+        // Function to create timer to animate tileSprite Game Objects
+    // Returns a reference to the newly added timer
+    createTileSpriteAnimTimer(obj, numTotalAnimFrames, fps = 4) {
+        let newTimer;
+        obj.setData("currAnimFrame", 0);
+        newTimer = this.time.addEvent(
+            {
+                delay: 1000/fps,
+                callback: () => {
+                    obj.setData("currAnimFrame", (obj.getData("currAnimFrame") + 1) % numTotalAnimFrames);
+                    obj.setFrame(obj.getData("currAnimFrame"));
+                },
+                loop: true
+            }
+        );
+        return newTimer;
     }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,6 +8,8 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
+        //load title
+        this.load.image("title", "./assets/Title.png");
         // Load background and lava
         this.load.spritesheet("volcanicBackground", "./assets/background sprite sheet (3 frames) 384x128.png", {frameWidth: 384/3, frameHeight: 128});
         this.load.spritesheet("lavaTop", "./assets/lava sprite sheet (4 frames) 512x50.png", {frameWidth: 512/4, frameHeight: 50});
@@ -17,24 +19,36 @@ class Menu extends Phaser.Scene {
         this.load.spritesheet("scientist", "./assets/running spritesheet 64 pixels a frame.png", {frameWidth: 64});
         this.load.spritesheet("scientistRunningSpritesheet", "./assets/running spritesheet 64 pixels a frame.png", {frameWidth: 64});
         // Load UI buttons
-        this.load.image("playButton", "./assets/janky play button.png");
+        this.load.image("playButton", "./assets/playButton.png");
+        this.load.image("creditsButton", "./assets/creditsButton.png")
+        this.load.image("titleButton", "./assets/titleButton.png")
         // Load audio assets
     }
 
     create() {
-        this.playButton = this.add.sprite(globalGameConfig.width/2, globalGameConfig.height/1.5, "playButton");
+        this.playButton = this.add.sprite(globalGameConfig.width/2, globalGameConfig.height/1.47, "playButton");
+        this.creditsButton = this.add.sprite(globalGameConfig.width/2, globalGameConfig.height/1.47 + this.playButton.height + this.playButton.height/6, "creditsButton");
+        this.title = this.add.sprite(globalGameConfig.width/2, globalGameConfig.height/4, "title");
         this.playButton.setInteractive();
+        this.creditsButton.setInteractive();
         // put menu scene to the top of screen
         this.scene.bringToTop();
-        // play button
+        // play button functionality
         this.playButton.on("pointerdown", () => {
             console.log("Clicked on playButton in Menu scene");
-            this.scene.stop("menuScene")
+            this.scene.sleep("menuScene")
             // start the game
             this.scene.get("playScene").startGameplay();
         });
+        //credits button functionality
+        this.creditsButton.on("pointerdown", () => {
+            console.log("Clicked on creditsButton in Menu scene");
+            this.scene.sleep("menuScene");
+            this.scene.sleep("playScene");
+            // start the game
+            this.scene.launch("creditsScene");
+        });
         this.scene.launch("playScene");
-
         // Assign events for when buttons are clicked
         upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
