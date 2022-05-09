769bf821986cdfb5a163bba3e3d2528e00266dc9
SalilPT <32131426+SalilPT@users.noreply.github.com>
2022-05-08 22:43:36 -0700

    Added information comment in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+/*
+//////////
+INFORMATION
+//////////
+
+Collaborators:
+Gustavo Cruz Martinez
+Miles Katlin
+Salil Tantamjarik
+
+Date Completed: May ???, 2022
+
+Game Title: "Jeb's Infernal Escape"
+
+Interesting Technical Features:
+- Used the launch() method run the Menu and Play scenes in parallel
+- Used object pooling for platforms
+- Used a render texture for the lava
+- Used a key combo to keep track of the inputted keys
+- Used lots of tweens
+
+Visual Style:
+Miles: This is the first time I have ever tried creating pixel art. I am quite 
+proud of all the art I made but I am particularly proud of the background and 
+main character Jeb. For the background, it took a lot of work to give it depth 
+while also making it seamless and I am very happy with how it turned out. Jeb 
+the robots animations were surprisingly difficult. I wanted to make sure each 
+animation could flow into the next and also wanted to make sure our hero stood 
+out from the chaos going on in the scene.
+*/
+
 // Naming convention: camel case
 // Line endings: LF
 
